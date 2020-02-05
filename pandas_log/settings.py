772f0cd1e7e2f6ccc33dcf65c23008082ea96919
Charles Davis <charles.m.davis.iv@gmail.com>
2020-02-05 13:52:21 -0800

    Removed __getitem__ logging since it was broken and not useful

--- pandas_log/settings.py
+++ pandas_log/settings.py
@@ -1,6 +1,6 @@
 ORIGINAL_METHOD_PREFIX = "original_"
 PATCHED_LOG_METHOD_PREFIX = "log_"
-DATAFRAME_ADDITIONAL_METHODS_TO_OVERIDE = ["copy", "reset_index", "__getitem__", "__setitem__"]
+DATAFRAME_ADDITIONAL_METHODS_TO_OVERIDE = ["copy", "reset_index", "__setitem__"]
 DATAFRAME_METHODS_TO_OVERIDE = [
     "query",
     "drop",
